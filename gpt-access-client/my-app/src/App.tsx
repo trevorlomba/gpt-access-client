ce2ba6e1b0f1bc02683db4acf460a812956a04cb
trevorlomba <trevorlomba@gmail.com>
2023-05-12 21:49:26 -0400

    styling

--- gpt-access-client/my-app/src/App.tsx
+++ gpt-access-client/my-app/src/App.tsx
@@ -5,32 +5,41 @@ import WordCloud from "./components/WordCloud";
 
 const App: React.FC = () => {
   const [animal, setAnimal] = useState("");
-  const [selectedWords, setSelectedWords] = useState<Array<string>>([]); // ["cat", "dog"]
+  const [selectedWords, setSelectedWords] = useState<{ text: string }[]>([])
+
 
   const [result, setResult] = useState<Array<string>>([]);
 
   
   const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    setResult([]); // Clear the previous results
-    for (let i = 0; i < 3; i++) {
-      try {
-        const formData = new URLSearchParams();
-        formData.append("animal", animal + " " + selectedWords.join(" "));
+		e.preventDefault()
+		setResult([]) // Clear the previous results
+		for (let i = 0; i < 3; i++) {
+			try {
+				const formData = new URLSearchParams()
 
-        const response = await axios.post("https://gpt-access-server.herokuapp.com/generate-response", formData, {
-          headers: {
-            "Content-Type": "application/x-www-form-urlencoded",
-          },
-        });
-        setResult((prevResult) => [...prevResult, response.data]);
-        console.log(response);
-      } catch (error) {
-        console.error("Error fetching data:", error);
-      }
-    }
-  };
+				// Map selectedWords to an array of strings, then join them
+				const selectedWordsString = selectedWords
+					.map((wordObj) => wordObj.text)
+					.join(' ')
+				formData.append('animal', animal + ' ' + selectedWordsString)
 
+				const response = await axios.post(
+					'https://gpt-access-server.herokuapp.com/generate-response',
+					formData,
+					{
+						headers: {
+							'Content-Type': 'application/x-www-form-urlencoded',
+						},
+					}
+				)
+				setResult((prevResult) => [...prevResult, response.data])
+				console.log(response)
+			} catch (error) {
+				console.error('Error fetching data:', error)
+			}
+		}
+	}
 
   const playTTS = (text: string) => {
     const synth = window.speechSynthesis;
--- gpt-access-client/my-app/src/components/WordCloud.tsx
+++ gpt-access-client/my-app/src/components/WordCloud.tsx
@@ -1,174 +1,378 @@
-import React, { useState } from "react";
-import wordsData from "../data/words.json";
-import styles from "../styles/WordCloud.module.css";
+import React, { useState } from 'react'
+import wordsData from '../data/words.json'
+import styles from '../styles/WordCloud.module.css'
 
 interface WordCloudProps {
-  selectedWords: string[];
-  setSelectedWords: React.Dispatch<React.SetStateAction<string[]>>;
-  playTTS: (text: string) => void;
-  handleSubmit: (e: React.FormEvent<HTMLFormElement>) => void;
+	selectedWords: { text: string; tag?: string }[]
+	setSelectedWords: React.Dispatch<
+		React.SetStateAction<{ text: string; tag?: string }[]>
+	>
+	playTTS: (text: string) => void
+	handleSubmit: (e: React.FormEvent<HTMLFormElement>) => void
 }
 
-const WordCloud: React.FC<WordCloudProps> = ({ selectedWords, setSelectedWords, playTTS, handleSubmit }) => {
-  const [selectedLetters, setSelectedLetters] = useState<string[]>([]);
-  const [clearLettersOnWordSelect, setClearLettersOnWordSelect] = useState(true);
+const WordCloud: React.FC<WordCloudProps> = ({
+	selectedWords,
+	setSelectedWords,
+	playTTS,
+	handleSubmit,
+}) => {
+	const [selectedLetters, setSelectedLetters] = useState<string[]>([])
+	const [clearLettersOnWordSelect, setClearLettersOnWordSelect] = useState(true)
+	const [currentTag, setCurrentTag] = useState<string | undefined>(undefined)
+	const [clearTagOnApply, setClearTagOnApply] = useState(true)
 
-  const handleAddWord = (val: string) => {
-    console.log(val);
-    if (selectedWords.includes(val)) {
-      setSelectedWords(selectedWords.filter((word) => word !== val));
+  const handleRemoveWord = (val: string) => {
+    handleAddWord(val)
+		let newWords = selectedWords.filter((wordObj) => wordObj.text !== val)
+
+		if (currentTag) {
+			let newVal = `${currentTag}: ${val}`
+			newWords = newWords.filter((wordObj) => wordObj.text !== newVal)
+		}
+    	if (clearTagOnApply) {
+				setCurrentTag('')
+			}
+
+		// setSelectedWords(newWords)
+	}
+
+	const handleAddWord = (val: string) => {
+	if (selectedWords.find((wordObj) => wordObj.text === val)) {
+    const wordObj = selectedWords.find((wordObj) => wordObj.text === val)
+    // if the word object has a tag value that is not undefined and not the current tag, then do not remove it but update the tag
+    if (wordObj && wordObj.tag !== currentTag) {
+      wordObj.tag = currentTag 
+      console.log(wordObj)
+      setSelectedWords([
+				...selectedWords.filter((wordObj) => wordObj.text !== val), wordObj])
+    
     } else {
-      setSelectedWords([...selectedWords, val]);
-      if (clearLettersOnWordSelect) {
-        setSelectedLetters([]);
-      }
-    }
-  };
+		setSelectedWords(
+			selectedWords.filter((wordObj) => wordObj.text !== val)
+		)}
+	} else {
+		setSelectedWords([...selectedWords, { text: val, tag: currentTag }])
+		setSelectedWords([...selectedWords, { text: val, tag: currentTag }])
+		if (clearLettersOnWordSelect) {
+			setSelectedLetters([])
+		}
+	
 
-  const handleLetterClick = (letter: string) => {
-    
-      setSelectedLetters([...selectedLetters, letter]);
-    
-  };
+	if (clearTagOnApply) {
+		setCurrentTag('')
+	}
+}
+	}
 
-    const handleAddSelectedLetters = () => {
-    const selectedWord = selectedLetters.join('').toLowerCase();
-    if (!selectedWords.includes(selectedWord)) {
-      setSelectedWords([...selectedWords, selectedWord]);
-    }
-    setSelectedLetters([]);
-  };
+	const handleAddSelectedLetters = () => {
+		const selectedWord = selectedLetters.join('').toLowerCase()
+		handleAddWord(selectedWord)
+		setSelectedLetters([])
+	}
 
-  const visibleWords = wordsData
-  .filter((word) =>
-    selectedLetters.length === 0 || word.text.startsWith(selectedLetters.join("").toLowerCase())
-  )
-  .sort((a, b) => b.frequency - a.frequency) // Sort by frequency in descending order
-  .slice(0, 100); // Keep only the top 20 words
+	const handleTagClick = (tag: string) => {
+    if (currentTag === tag) {
+      setCurrentTag('')
+		} else {
+      setCurrentTag(tag)
+		}
+	}
 
+// const checkPrefixes = (word: string) => {
+// 	for (const tag of tags) {
+// 		if (word.startsWith(`${tag}:`)) {
+// 			const newWord = word.replace(`${tag}:`, ``)
+// 			setSelectedWords(selectedWords.filter((wordObj) => wordObj.text !== word))
+// 			console.log(word)
+// 			console.log(newWord)
+// 			return newWord
+// 		}
+// 	}
+// 	return word
+// }
 
-  const minFrequency = Math.min(...visibleWords.map((word) => word.frequency));
-  const maxFrequency = Math.max(...visibleWords.map((word) => word.frequency));
+	const handleLetterClick = (letter: string) => {
+		setSelectedLetters([...selectedLetters, letter])
+	}
 
-  const computeFontSize = (frequency: number): number => {
-    const minFontSize = 20;
-    const maxFontSize = 70;
-    return minFontSize + (((frequency - minFrequency) / (maxFrequency - minFrequency)) * (maxFontSize - minFontSize));
-  };
+	// const handleAddSelectedLetters = () => {
+	// 	const selectedWord = selectedLetters.join('').toLowerCase()
+	// 	if (!selectedWords.includes(selectedWord)) {
+	// 		setSelectedWords([...selectedWords, selectedWord])
+	// 	}
+	// 	setSelectedLetters([])
+	// }
 
-  const tags = ["tag1", "tag2", "tag3", "tag4", "tag5", "tag6", "tag7", "tag8"]
+	const visibleWords = wordsData
+		.filter(
+			(word) =>
+				selectedLetters.length === 0 ||
+				word.text.startsWith(selectedLetters.join('').toLowerCase())
+		)
+		.sort((a, b) => b.frequency - a.frequency) // Sort by frequency in descending order
+		.slice(0, 100) // Keep only the top 20 words
 
-//   const color = () => {
-//     return '#' + Math.floor(Math.random()*16777215).toString(16);
-//   };
+	const minFrequency = Math.min(...visibleWords.map((word) => word.frequency))
+	const maxFrequency = Math.max(...visibleWords.map((word) => word.frequency))
 
-  const alphabet = "QWERTYUIOPASDFGHJKLZXCVBNM".split("");
-  const numbers = "12345676890".split("");
+	const computeFontSize = (frequency: number): number => {
+		const minFontSize = 20
+		const maxFontSize = 70
+		return (
+			minFontSize +
+			((frequency - minFrequency) / (maxFrequency - minFrequency)) *
+				(maxFontSize - minFontSize)
+		)
+	}
 
-  return (
-    <div style={{ display: 'flex', flexDirection: 'column', height: '100%' }}>
-      <div style={{ flex: 1, overflow: '', height: "100%"}}>
-        <div style={{height:"4rem"}}>{selectedWords.map((val, i) => (
-          <button key={i} value={val} onClick={() => handleAddWord(val)} style={{fontSize: "1rem", margin: "5px", marginBottom: "10px", padding: "10px"}}>
-            {val}
-          </button>
-        ))}</div>
-        
-          <div>
-            <button style = {{backgroundColor: "wheat", fontSize: "calc(.7vw + 1vh)",  padding: "10px", width: "48%", maxHeight: "5%"}} onClick={() => setSelectedWords([])}>Clear Words</button>
-        <button style={{backgroundColor: "green", color:"white", fontSize: "calc(.7vw + 1vh)",  padding: "10px", fontWeight:"bolder",  width: "48%"}}onClick={() => playTTS(selectedWords.toString())}>Play Selected Words</button>
+	const tags = ['subject', 'object', 'adverb']
 
-          </div>
+	//   const color = () => {
+	//     return '#' + Math.floor(Math.random()*16777215).toString(16);
+	//   };
 
-        <div>
-          <button style={{ margin: "1px", padding: "10px", fontSize: "calc(.7vw + 1vh)", backgroundColor: "lightgrey",  width: "30%"}} onClick={() => setSelectedLetters([]) }>Clear  Letters</button>
-          <button style={{ margin: "1px", padding: "10px", fontSize: "calc(.7vw + 1vh)", backgroundColor: "indianred", color: "whitesmoke",  width: "30%"}} onClick={() => setSelectedLetters(selectedLetters.slice(0, -1))}>Backspace</button>
-          <button style={{ margin: "1px", padding: "10px", fontSize: "calc(.7vw + 1vh)", backgroundColor: "cornflowerblue", marginBottom: "calc(2vh)",  color: "whitesmoke", width: "30%"}}onClick={handleAddSelectedLetters}>Add Word</button>
-         
-        </div>
-      </div>
-      <div style={{ flex: 1.5, overflow: '' , margin:"1vw"}}>
-         <div>
-        <label>
-          <input
-            type="checkbox"
-            checked={clearLettersOnWordSelect}
-            style={{marginBottom: "20px"}}
-            onChange={() => setClearLettersOnWordSelect(!clearLettersOnWordSelect)}
-          />
-          Clear letters when a word is chosen
-        </label>
-      </div>
-   <div>
-         {numbers.map((number) => (
-          <>
-          <button
-            key={number}
-            onClick={() => handleLetterClick(number)}
-            style={{ width: "calc(8vw)", paddingLeft: "calc(.7vw)", paddingRight: "calc(.7vw)", fontSize:"calc(1vw + 1vh)", padding: "calc(.5vh)", borderRadius: "20%", color: "darkgreen", fontWeight: "bolder", backgroundColor: "lightgreen"}}
-          >
-            {number}
+	const alphabet = 'QWERTYUIOPASDFGHJKLZXCVBNM'.split('')
+	const numbers = '12345676890'.split('')
 
-          </button>
-          {number === "0" && <br></br>}
-          </>
-        ))}
-        {alphabet.map((letter) => (<>
-          <button
-            key={letter}
-            onClick={() => handleLetterClick(letter)}
-            style={{ width: "calc(9vw)" ,paddingLeft: "calc(.7vw)", paddingRight: "calc(.7vw)", fontSize: "calc(1vw + 1vh)", padding: "calc(.5vh)", borderRadius: "20%", backgroundColor: "lightblue", color: "darkblue"}}
-            >
-            {letter}
-          </button>
-          {letter === "P" && <br></br>}
-          {letter === "L" && <br></br>}
-          {letter === "M" && <br></br>}
-            </>
-        ))}</div>
-           <div>
-            {/* map a new button for reach item in array tags */}
-            {tags.map((tag) => (
-              <button
+	return (
+		<div style={{ display: 'flex', flexDirection: 'column', height: '100%' }}>
+			<div style={{ flex: 1, overflow: '', height: '100%' }}>
+				<div>
+					<label>
+						<input
+							type='checkbox'
+							checked={clearLettersOnWordSelect}
+							style={{ marginBottom: '1rem' }}
+							onChange={() =>
+								setClearLettersOnWordSelect(!clearLettersOnWordSelect)
+							}
+						/>
+						Clear letters when a word is chosen
+					</label>
+					<label>
+						<input
+							type='checkbox'
+							checked={clearTagOnApply}
+							onChange={() => setClearTagOnApply(!clearTagOnApply)}
+						/>
+						Clear tag when applied
+					</label>
+				</div>
+				<div
+					style={{ marginTop: 'auto', marginBottom: 'auto', height: '3rem' }}>
+					{selectedWords.map((wordObj, i) => (
+						<button
+							key={i}
+							value={wordObj.text}
+							onClick={() => handleRemoveWord(wordObj.text)}
+							style={{
+								fontSize: '1rem',
+								alignSelf: 'center',
+								marginTop: 'auto',
+								marginBottom: 'auto',
+								padding: '10px',
+							}}>
+							{wordObj.tag ? `${wordObj.tag}: ${wordObj.text}` : wordObj.text}
+						</button>
+					))}
+				</div>
 
-                key={tag}
-                // onClick={() => handleLetterClick(tag)}
-                style={{ width: `calc(90vw/${tags.length})`, paddingLeft: "calc(.7vw)", paddingRight: "calc(.7vw)", fontSize:"auto", padding: "calc(.5vh)", borderRadius: "20%", color: "darkred", fontWeight: "bolder", backgroundColor: "pink"}}
-              >
-                {tag}
-              </button>
-            ))}</div>
-            </div>
-        <div style={{ flex: 2, overflow: 'auto' , margin:"1vw"}}>
-         <span style={{ fontSize: "50px", color: "blueviolet"}}>{selectedLetters}</span>
-        {visibleWords
-          .sort((a, b) => a.text.localeCompare(b.text))
-          .map((word) => (
-            <div
-              key={word.id}
-              onClick={() => handleAddWord(word.text)}
-              className={styles.word}
-              style={{
-                fontSize: `${computeFontSize(word.frequency)}px`,
-                // border: `${(computeFontSize(word.frequency)/15)}px solid darkgrey`,
-                backgroundColor: "lightgrey",
-                borderRadius: "20%",
-                padding: "5px 10px",
-                margin: "5px",
-                alignContent: "center",
-                color: "black",
-                display: "inline-block",
-                paddingTop: "20px",
-                paddingBottom: "20px",
-                height: "auto",
-                filter: `drop-shadow(0 ${(computeFontSize(word.frequency)/3)}px ${(computeFontSize(word.frequency)/5)}px grey)`
-              }}
-            >
-              {word.text}
-            </div>
-          ))}
-      </div>
-    </div>
-  ); }
+				<div>
+					<button
+						style={{
+							backgroundColor: 'wheat',
+							fontSize: 'calc(.7vw + 1vh)',
+							padding: '10px',
+							width: '30%',
+							maxHeight: '5%',
+						}}
+						onClick={() => setSelectedWords([])}>
+						Clear Words
+					</button>
+					<button
+						style={{
+							backgroundColor: 'green',
+							color: 'white',
+							fontSize: 'calc(.7vw + 1vh)',
+							padding: '10px',
+							fontWeight: 'bolder',
+							width: '30%',
+						}}
+						onClick={() => playTTS(selectedWords.toString())}>
+						Play Selected Words
+					</button>
+					<button
+						style={{
+							margin: '1px',
+							padding: '10px',
+							fontSize: 'calc(.7vw + 1vh)',
+							backgroundColor: 'lightgrey',
+							width: '30%',
+						}}
+						onClick={() => setSelectedLetters([])}>
+						Clear Letters
+					</button>
+				</div>
 
-export default WordCloud;
+				<div></div>
+			</div>
+			<div style={{ flex: 1.5, overflow: '', margin: '1vw' }}>
+				<div>
+					{tags.map((tag) => (
+						<button
+							key={tag}
+							onClick={() => handleTagClick(tag)}
+							style={{
+								width: `calc(90vw/${tags.length + 2})`,
+								paddingLeft: 'calc(.7vw)',
+								paddingRight: 'calc(.7vw)',
+								fontSize: 'auto',
+								padding: 'calc(1vh)',
+								borderRadius: '10%',
+								color: currentTag === tag ? 'white' : 'darkred',
+								fontWeight: 'bolder',
+								backgroundColor: currentTag === tag ? 'grey' : 'white',
+							}}>
+							{tag}
+						</button>
+					))}
+					<button
+						style={{
+							margin: '1px',
+							width: `calc(90vw/(calc(${tags.length + 2})))`,
+							paddingLeft: 'calc(.7vw)',
+							padding: 'calc(1vh)',
+							borderRadius: '10%',
+							fontWeight: 'bolder',
+							paddingRight: 'calc(.7vw)',
+							fontSize: 'auto',
+							backgroundColor: 'indianred',
+							color: 'whitesmoke',
+						}}
+						onClick={() => setSelectedLetters(selectedLetters.slice(0, -1))}>
+						Backspace
+					</button>
+					<button
+						style={{
+							margin: '1px',
+							width: `calc(90vw/(calc(${tags.length + 2})))`,
+							padding: 'calc(1vh)',
+							borderRadius: '10%',
+							fontWeight: 'bolder',
+							paddingLeft: 'calc(.7vw)',
+							paddingRight: 'calc(.7vw)',
+							fontSize: 'auto',
+							backgroundColor: 'cornflowerblue',
+							marginBottom: 'calc(2vh)',
+							color: 'whitesmoke',
+						}}
+						onClick={handleAddSelectedLetters}>
+						Add Word
+					</button>
+				</div>
+				<div>
+					{numbers.map((number) => (
+						<>
+							<button
+								key={number}
+								onClick={() => handleLetterClick(number)}
+								style={{
+									width: 'calc(8vw)',
+									paddingLeft: 'calc(.7vw)',
+									paddingRight: 'calc(.7vw)',
+									fontSize: 'calc(1vw + 1vh)',
+									padding: 'calc(1vh)',
+									borderRadius: '20%',
+									color: 'darkgreen',
+									fontWeight: 'bolder',
+									backgroundColor: 'lightgreen',
+								}}>
+								{number}
+							</button>
+							{number === '0' && <br></br>}
+						</>
+					))}
+					{alphabet.map((letter) => (
+						<>
+							<button
+								key={letter}
+								onClick={() => handleLetterClick(letter)}
+								style={{
+									width: 'calc(9vw)',
+									paddingLeft: 'calc(.7vw)',
+									paddingRight: 'calc(.7vw)',
+									fontSize: 'calc(1vw + 1vh)',
+									padding: 'calc(1.5vh)',
+									borderRadius: '20%',
+									backgroundColor: 'lightblue',
+									color: 'darkblue',
+									height: '120%',
+								}}>
+								{letter}
+							</button>
+							{letter === 'P' && <br></br>}
+							{letter === 'L' && <br></br>}
+							{letter === 'M' && <br></br>}
+						</>
+					))}
+				</div>
+				<div></div>
+			</div>
+			<div style={{ flex: 2, overflow: 'auto', margin: '1vw' }}>
+				<div
+					onClick={() => handleAddWord(selectedLetters.join('').toLowerCase())}
+					className={styles.word}
+					style=
+					{{
+						fontSize: `calc(90vw/12)`,
+						// border: `${(computeFontSize(word.frequency)/15)}px solid darkgrey`,
+						backgroundColor: 'lightcoral',
+						borderRadius: '20%',
+						padding: '5px 10px',
+						margin: '5px',
+            marginTop: 'auto',
+						alignContent: 'center',
+						color: 'black',
+						display: 'inline-block',
+						paddingTop: '20px',
+						paddingBottom: '20px',
+						height: 'auto',
+						filter: `drop-shadow(0 90px 90px grey)`,
+					}}>
+						{selectedLetters}
+			
+				</div>
+				{visibleWords
+					.sort((a, b) => a.text.localeCompare(b.text))
+					.map((word) => (
+						<div
+							key={word.id}
+							onClick={() => handleAddWord(word.text)}
+							className={styles.word}
+							style={{
+								fontSize: `calc(${computeFontSize(word.frequency)}vw/12)`,
+								// border: `${(computeFontSize(word.frequency)/15)}px solid darkgrey`,
+								backgroundColor: 'lightgrey',
+								borderRadius: '20%',
+								padding: '5px 10px',
+								margin: '5px',
+								alignContent: 'center',
+								color: 'black',
+								display: 'inline-block',
+								paddingTop: '20px',
+								paddingBottom: '20px',
+								height: 'auto',
+								filter: `drop-shadow(0 ${
+									computeFontSize(word.frequency) / 3
+								}px ${computeFontSize(word.frequency) / 5}px grey)`,
+							}}>
+							{word.text}
+						</div>
+					))}
+			</div>
+		</div>
+	)
+}
+
+export default WordCloud
